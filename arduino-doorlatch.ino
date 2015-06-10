4475e82af0f6e40bd267d473e8295ac962d0abff
Nick Hodulik <nick@hodulik.com>
2015-06-09 17:08:12 -0700

    Changed API so it takes an argument for how long to hold the door open

--- arduino-doorlatch.ino
+++ arduino-doorlatch.ino
@@ -8,8 +8,11 @@ YunServer server;
 
 void setup() {
   // Bridge startup
+  // Connect the switch lead to pin 12 for control
   pinMode(12, OUTPUT);
   digitalWrite(12, LOW);
+  // pin 13 is the onboard LED, for feedback
+  // you can also control the pin itself, but it doubles up with the LED
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
   Bridge.begin();
@@ -35,6 +38,11 @@ void loop() {
   delay(50); // Poll every 50ms
 }
 
+//set up a way to process URL's
+
+//with the below we want the URL to be
+//http://doorlatch.local/arduino/buzz/{seconds_to_hold_open}
+//so we set up "buzz" as the handler
 void process(YunClient client) {
   // read the command
   String command = client.readStringUntil('/');
@@ -45,41 +53,54 @@ void process(YunClient client) {
   }
 }
 
+//takes one mandatory argument of the number of seconds to hold the door open
+//valid values are between 1 and 10 seconds
 void buzzCommand(YunClient client) {
-  int trigger;
+  int seconds_to_hold_open;
+  String jsonp_return;
   
-  // Read whether to trigger or not
-  // only accepts 1; any other value doesn't do anything
-  trigger = client.parseInt();
+  // Read seconds_to_hold_open
+  seconds_to_hold_open = client.parseInt();
   
-  if (trigger == 1) {
-    //open the doorlatch
-    digitalWrite(12, HIGH);
-    //shine the onboard LED for shits and giggles
-    digitalWrite(13, HIGH);
-    /*
-      originally here I had the following line
-        client.println("Door latch open");
-      but this isn't synchronous communication
-      the Yun sends all of the data back to the client at once at the end of this function
-      as such, it's not useful for realtime status and only for informing the end user of final info
-    */
-    //keep buzzing the latch open for 7s
-    delay(7000);
-    /*
-      note that the default web server timeout on the Yun is 5 seconds
-      the arduino will faithfully execute this code even with the timeout
-      but the Lua webserver will return a 500 error to the requesting web client if it times out
-      which will probably screw your web client code up if it is properly checking for HTTP status
-      you thus need to make sure your socket_timeout in /etc/config/arduino is greater than the delay above
-    */
-    //close the doorlatch
-    digitalWrite(12, LOW);
-    //turn off the LED
-    digitalWrite(13, LOW);
-    //send the client final status message
-    client.println("Success");
+  //check the length of the hold
+  //valid lengths are between 1 and 10s
+  //if it is longer than 10s, set to 10s
+  if (seconds_to_hold_open > 10) {
+     seconds_to_hold_open = 10;
   }
-
+  
+  //if it is negative set it to 1
+  if (seconds_to_hold_open < 0) {
+     seconds_to_hold_open = 1;
+  }
+  
+  //open the doorlatch
+  digitalWrite(12, HIGH);
+  //shine the onboard LED for shits and giggles
+  digitalWrite(13, HIGH);
+  
+  //keep buzzing the latch open for seconds_to_hold_open
+  //delay() takes its argument in ms so we x1000
+  delay(seconds_to_hold_open * 1000);
+  
+  /*
+    note that the default web server timeout on the Yun is 5 seconds
+    the arduino will faithfully execute this code even with the timeout
+    but the Lua webserver will return a 500 error to the requesting web client if it times out
+    which will probably screw your web client code up if it is properly checking for HTTP status
+    you thus need to make sure your socket_timeout in /etc/config/arduino is greater than the delay above
+  */
+  //close the doorlatch
+  digitalWrite(12, LOW);
+  //turn off the LED
+  digitalWrite(13, LOW);
+  //send the client final status message
+  //construct string first since println doesn't like concatenation
+  //and compiler does weird things
+  //sending it as JSONP so CORS issues don't come up
+  jsonp_return = "seconds_held_open({\"value\": ";
+  jsonp_return += seconds_to_hold_open;
+  jsonp_return += "});";
+  client.println(jsonp_return);
 }
 
